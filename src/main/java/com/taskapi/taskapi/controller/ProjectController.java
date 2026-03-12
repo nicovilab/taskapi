eac52dc5189c49759e2583fd323f2e6785e563a6
Nico <nicolas.vila.besada@gmail.com>
2026-03-12 13:27:29 +0100

    project controller

---
+++ src/main/java/com/taskapi/taskapi/controller/ProjectController.java
@@ -0,0 +1,49 @@
+package com.taskapi.taskapi.controller;
+
+import com.taskapi.taskapi.dto.project.ProjectRequest;
+import com.taskapi.taskapi.dto.project.ProjectResponse;
+import com.taskapi.taskapi.service.ProjectService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+
+    private ProjectService projectService;
+
+    @GetMapping
+    public ResponseEntity<List<ProjectResponse>> findAll(Authentication authentication){
+        return ResponseEntity.ok(projectService.findAllByUser(authentication.getName()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProjectResponse> findById(@PathVariable Long id, Authentication authentication){
+        return ResponseEntity.ok(projectService.findById(id, authentication.getName()));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProjectResponse> create(@Valid @RequestBody ProjectRequest request, Authentication authentication){
+        return ResponseEntity.status(HttpStatus.CREATED).body(projectService.create(request, authentication.getName()));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProjectResponse> update(@PathVariable Long id, @Valid @RequestBody ProjectRequest request, Authentication authentication){
+        return ResponseEntity.ok(projectService.update(id,request, authentication.getName()));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id, Authentication authentication){
+        projectService.delete(id, authentication.getName());
+        return ResponseEntity.noContent().build();
+    }
+}
