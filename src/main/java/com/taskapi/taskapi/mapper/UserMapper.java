9f4d8fc1129e1efc669621af2539a4ed25a669eb
Nico <nicolas.vila.besada@gmail.com>
2026-03-16 17:15:35 +0100

    add user mapper

---
+++ src/main/java/com/taskapi/taskapi/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.taskapi.taskapi.mapper;
+
+import com.taskapi.taskapi.dto.auth.UserResponse;
+import com.taskapi.taskapi.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "roles", expression = "java(user.getRoles().stream().map(Enum:name).collect(java.util.stream.Collectors.toSet()))")
+    UserResponse toResponse(User user);
+}
