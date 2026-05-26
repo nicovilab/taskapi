44455972e656b9be391b5da05913fd3d08dc8571
Nico <nicolas.vila.besada@gmail.com>
2026-05-26 19:12:24 +0200

    add a rate limit filter

---
+++ src/main/java/com/taskapi/taskapi/security/RateLimitFilter.java
@@ -0,0 +1,46 @@
+package com.taskapi.taskapi.security;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import jakarta.annotation.Priority;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Priority(1)
+public class RateLimitFilter extends OncePerRequestFilter {
+
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    private Bucket createBucket() {
+        return Bucket.builder()
+                .addLimit(Bandwidth.builder()
+                        .capacity(10)
+                        .refillGreedy(10, Duration.ofMinutes(1))
+                        .build())
+                .build();
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String ip = request.getRemoteAddr();
+        Bucket bucket = buckets.computeIfAbsent(ip, k -> createBucket());
+
+        if (bucket.tryConsume(1)){
+            filterChain.doFilter(request, response);
+        }else{
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            response.getWriter().write("Too many requests");
+        }
+    }
+}
